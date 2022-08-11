79acb07d42ab6f47b7762e4c574aabfd88e23c58
Michael Wood <michaelwood33311@icloud.com>
2022-08-11 13:16:22 -0400

    simple loop for monitoring page response time

---
+++ index.js
@@ -0,0 +1,37 @@
+const waitSeconds = 3;
+
+const axios = require('axios');
+const fs = require('fs');
+
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+const checkResponseTime = async () => {
+    let rawdata = fs.readFileSync('urlList.json');
+    const urlList = JSON.parse(rawdata);
+    let start, end, response, request;
+
+    while(true) {
+        for (let i = 0; i < urlList.length; ++i) {
+            request = {
+                url: urlList[i],
+                method: 'get',
+                timeout: 10000
+            }
+            try {
+                start = Date.now();
+                response = await axios(request);
+                end = Date.now();
+                console.log(`${end - start}ms: ${request.url}`);
+            } catch (err) {
+                console.log(`Axios Error ${err.message}: cannot fetch ${request.url}`);
+            }
+
+            await sleep(waitSeconds * 1000);
+        }
+
+        
+    }    
+}
+
+checkResponseTime();
